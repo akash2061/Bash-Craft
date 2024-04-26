ae5bfe05e77ac3c9bb5bf9024c9bd11ee8a42864
akash2061 <lucifermorningstar2061@gmail.com>
2024-04-26 22:10:34 +0530

    Switch-Case

--- switch-case.sh
+++ switch-case.sh
@@ -13,11 +13,11 @@ echo "7. Something else...?"
 read d;
 
 case $d in
-    1) echo "I use Arch BTW! :)"
-    2) echo "BlackArch is a pentesting distribution based on Arch."
-    3) echo "Kali-Linux is a pentesting & hacking linux system."
-    4) echo "Parrot-OS is a pentesting & hacking linux system with HTB integrated."
-    5) echo "Ubuntu is a basic user-friendly Destribution for new users."
-    6) echo "Debian is a community destribution."
-    7) echo "You didn't exter an appropriate choice."
+    1) echo "I use Arch BTW! :)";;
+    2) echo "BlackArch is a pentesting distribution based on Arch.";;
+    3) echo "Kali-Linux is a pentesting & hacking linux system.";;
+    4) echo "Parrot-OS is a pentesting & hacking linux system with HTB integrated.";;
+    5) echo "Ubuntu is a basic user-friendly Destribution for new users.";;
+    6) echo "Debian is a community destribution.";;
+    7) echo "You didn't exter an appropriate choice.";;
 esac
