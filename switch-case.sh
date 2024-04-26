7f4e129c16d8960686fd1bf7dfb69069173e7e40
akash2061 <lucifermorningstar2061@gmail.com>
2024-04-26 22:00:23 +0530

    Switch-Case

---
+++ switch-case.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "What is your favourite Linux Destribution?"
+
+echo "1. Arch-Linux"
+echo "2. Black-Arch-Linux"
+echo "3. Kali-Linux"
+echo "4. Parrot-OS"
+echo "5. Ubuntu"
+echo "6. Debian"
+echo "7. Something else...?"
+
