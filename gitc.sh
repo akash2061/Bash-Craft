6f5ee1d1098dcc88ab753272252a7ed4fa3f93a0
akash2061 <lucifermorningstar2061@gmail.com>
2024-04-27 09:49:37 +0530

    git Helper.

--- gitc.sh
+++ gitc.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-read -p "Name: " name
-read -p "Repository: " repo
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-cmd="git clone https://github.com/$name/$repo"
+# Prompt for Name and Repository with colored output
+read -p "${GREEN}Name: ${NC}" name
+read -p "${GREEN}Repository: ${NC}" repo
 
-$cmd
+# Construct the git clone command
+cmd="git clone https://github.com/'$name'/'$repo'"
+
+# Execute the command using eval to handle complex scenarios better
+eval "$cmd"
+
+# Check if the git clone command was successful
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Repository cloned successfully.${NC}"
+else
+    echo -e "${RED}Failed to clone repository.${NC}"
+fi
+
