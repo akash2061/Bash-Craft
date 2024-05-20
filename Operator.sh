90149469816444fe46d5812d24cc94206ea33025
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-20 15:06:13 +0530

    foo

--- Operator.sh
+++ Operator.sh
@@ -4,5 +4,18 @@ read -p "Enter number 2:" b
 add=$((a+b))
 
 echo "Add = $add"
+echo ""
 
 echo "Conditional Statement:"
+read -p "Enter number 3:" c
+echo ""
+
+if [ $a -gt $b -a $a -gt $c ];
+then
+    echo "A is largest Number"
+elif [ $b -gt $a  -a $b -gt $c ];
+then
+    echo "B is largest Number"
+else
+    echo "C is largest Number"
+fi
